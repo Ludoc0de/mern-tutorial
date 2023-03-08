8e182739fd9c0b16a9da46cd35d87ba7094805a1
ludoc0de <champi.ludo@tutanota.com>
2023-03-08 13:36:13 -0300

    up

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -2,12 +2,12 @@ import { useDispatch } from "react-redux";
 import { deleteGoal } from "../features/goals/goalSlice";
 
 function GoalItem({ goal }) {
-  console.log(goal);
+  console.log("GoalItem", goal);
   const dispatch = useDispatch();
 
   return (
     <div className="goal">
-      {/* <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div> */}
+      <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
       <h2>{goal.text}</h2>
       <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
         X
