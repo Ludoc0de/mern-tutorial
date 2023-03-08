12d83bf30f807eb114ae4bc0e3b2400e7f90b722
ludoc0de <champi.ludo@tutanota.com>
2023-03-08 20:23:31 -0300

    up

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -3,13 +3,14 @@ import { deleteGoal } from "../features/goals/goalSlice";
 
 function GoalItem({ goal }) {
   console.log("GoalItem", goal);
+  console.log("GoalItem", goal.subText, goal.subText2);
   const dispatch = useDispatch();
 
   return (
     <div className="goal">
       <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
-      <h2>{goal.text}</h2>
-      <h2>{goal.subText}</h2>
+      {/* <h2>{goal.text}</h2> */}
+      <p>{goal.subText}</p>
       <h2>{goal.subText2}</h2>
       <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
         X
